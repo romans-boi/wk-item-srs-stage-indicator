058f00779fb3885d5301b96095797064dcddbbf0
Romans Kuznecovs <kuznecovs.romans@gmail.com>
2025-12-07 12:27:25 +0000

    Use event to determine the current item id

--- src/item_srs_stage_indicator_script.js
+++ src/item_srs_stage_indicator_script.js
@@ -1,94 +1,152 @@
 // ==UserScript==
 // @name         WaniKani Item SRS Stage Indicator
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.2.0
 // @description  Displays the exact item SRS stage (Apprentice 3, Guru 1, etc.), both before and after completing the review for the item.
 // @author       romans-boi
-// @match        https://www.wanikani.com/subjects/review
+// @match        https://www.wanikani.com/*
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
 (async function () {
-    // Correct namings for the stages
-    const CURRENT_NAMES = ["Unlocked", "Apprentice", "Apprentice", "Apprentice", "Apprentice", "Guru", "Guru", "Master", "Enlightened", "Burned"];
-    const CORRECTED_NAMES = ["Unlocked", "Apprentice I", "Apprentice II", "Apprentice III", "Apprentice IV", "Guru I", "Guru II", "Master", "Enlightened", "Burned"];
+  /* global Stimulus */
 
-    const SUBJECT_IDS_WITH_SRS_TARGET = "subjectIdsWithSRS";
-    const SUBJECTS_TARGET = "subjects";
+  // Correct namings for the stages
+  const CURRENT_NAMES = [
+    "Unlocked",
+    "Apprentice",
+    "Apprentice",
+    "Apprentice",
+    "Apprentice",
+    "Guru",
+    "Guru",
+    "Master",
+    "Enlightened",
+    "Burned",
+  ];
+  const CORRECTED_NAMES = [
+    "Unlocked",
+    "Apprentice I",
+    "Apprentice II",
+    "Apprentice III",
+    "Apprentice IV",
+    "Guru I",
+    "Guru II",
+    "Master",
+    "Enlightened",
+    "Burned",
+  ];
 
-    let queueElement;
-    let parentElement;
-    let clonedQueueElement;
+  const SUBJECT_IDS_WITH_SRS_TARGET = "subjectIdsWithSRS";
 
-    init();
+  let queueElement;
+  let parentElement;
+  let clonedQueueElement;
 
-    function init() {
-        // As recommended by Tofugu Scott: https://community.wanikani.com/t/updates-to-lessons-reviews-and-extra-study/60912/28
-        queueElement = document.getElementById('quiz-queue');
-        parentElement = queueElement.parentElement;
-        queueElement.remove();
-        clonedQueueElement = queueElement.cloneNode(true);
+  window.addEventListener("turbo:load", (event) => {
+    // If we're not on the review page, then ignore
+    if (!/subjects\/review/.test(event.detail.url)) return;
 
-        addStyle();
-        replaceSrsStageNames();
+    waitForQuizQueue(init);
+  });
 
-        addDidChangeSrsListener()
-        addNextQuestionEventListener()
+  function waitForQuizQueue(callback) {
+    // Need to make sure we wait for quiz queue to be available first
+    const observer = new MutationObserver((mutations, observer) => {
+      const element = document.getElementById("quiz-queue");
+      if (element) {
+        observer.disconnect();
+        callback();
+      }
+    });
 
-        parentElement.appendChild(clonedQueueElement)
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+
+  function init() {
+    // As recommended by Tofugu Scott: https://community.wanikani.com/t/updates-to-lessons-reviews-and-extra-study/60912/28
+    queueElement = document.getElementById("quiz-queue");
+    parentElement = queueElement.parentElement;
+    clonedQueueElement = queueElement.cloneNode(true);
+    queueElement.remove();
+
+    addStyle();
+    replaceSrsStageNames();
+
+    addDidChangeSrsListener();
+    addNextQuestionEventListener();
+
+    parentElement.appendChild(clonedQueueElement);
+  }
+
+  function replaceSrsStageNames() {
+    // Get the element that holds the SRS names, and replace them with the corrected ones.
+    const srsStagesElement = clonedQueueElement.querySelector(
+      `[data-quiz-queue-target="subjectIdsWithSRS"]`
+    );
+    const srsStagesText = srsStagesElement.textContent;
+    const srsStagesCorrectedText = srsStagesText.replaceAll(
+      JSON.stringify(CURRENT_NAMES),
+      JSON.stringify(CORRECTED_NAMES)
+    );
+    srsStagesElement.textContent = srsStagesCorrectedText;
+  }
+
+  function showCurrentSrsContainer() {
+    document.querySelector(
+      ".character-header__current-srs-container"
+    ).dataset.hidden = "false";
+  }
+
+  function hideCurrentSrsContainer() {
+    document.querySelector(
+      ".character-header__current-srs-container"
+    ).dataset.hidden = "true";
+  }
+
+  function addDidChangeSrsListener() {
+    window.addEventListener("didChangeSRS", (e) => {
+      hideCurrentSrsContainer();
+    });
+  }
+
+  function addNextQuestionEventListener() {
+    window.addEventListener("willShowNextQuestion", (event) => {
+      onNextQuestion(event.detail.subject);
+    });
+
+    function onNextQuestion(subject) {
+      setCurrentSrsStageText(subject);
+      showCurrentSrsContainer();
     }
 
-    function replaceSrsStageNames() {
-        // Get the element that holds the SRS names, and replace them with the corrected ones.
-        const srsStagesElement = getQuizQueueTarget(SUBJECT_IDS_WITH_SRS_TARGET);
-        const srsStagesText = srsStagesElement.textContent;
-        const srsStagesCorrectedText = srsStagesText.replaceAll(JSON.stringify(CURRENT_NAMES), JSON.stringify(CORRECTED_NAMES));
-        srsStagesElement.textContent = srsStagesCorrectedText;
-    }
+    function setCurrentSrsStageText(subject) {
+      const currentItemId = subject.id;
 
-    function showCurrentSrsContainer() {
-        document.querySelector('.character-header__current-srs-container').dataset.hidden = "false";
-    }
+      // Get SRS stages config
+      const srsStagesJson = JSON.parse(
+        getQuizQueueTarget(SUBJECT_IDS_WITH_SRS_TARGET).textContent
+      );
+      const subjectIdSrsList = srsStagesJson.subject_ids_with_srs_info;
 
-    function hideCurrentSrsContainer() {
-        document.querySelector('.character-header__current-srs-container').dataset.hidden = "true";
-    }
+      // The tuple we get for a subject looks like this `[id, index, X]` where X is pointing to which array of SRS names to use within
+      // srs_ids_stage_names, but from what I can tell... it doesn't matter? So I just use the local corrected names array for lookup.
+      const currentItemSrsIndex = subjectIdSrsList.find(
+        (subjectSrsInfo) => subjectSrsInfo[0] == currentItemId
+      )[1];
+      const currentSrsStageName = CORRECTED_NAMES[currentItemSrsIndex];
 
-    function addDidChangeSrsListener() {
-        window.addEventListener("didChangeSRS", () => hideCurrentSrsContainer());
-    }
+      const currentSrsTextDiv = document.querySelector(
+        `div[class='character-header__current-srs-text']`
+      );
 
-    function addNextQuestionEventListener() {
-        window.addEventListener("willShowNextQuestion", () => {
-            onNextQuestion();
-        });
-        onNextQuestion();
-
-        function onNextQuestion() {
-            setCurrentSrsStageText();
-            showCurrentSrsContainer();
-        }
-
-        function setCurrentSrsStageText() {
-            // Get SRS stages config
-            const srsStagesJson = JSON.parse(getQuizQueueTarget(SUBJECT_IDS_WITH_SRS_TARGET).textContent);
-            const subjectIdSrsList = srsStagesJson.subject_ids_with_srs_info;
-
-            // Get current queue config
-            const queueJson = JSON.parse(getQuizQueueTarget(SUBJECTS_TARGET).textContent);
-            const currentItemId = queueJson[0].id;
-
-            // The tuple we get for a subject looks like this `[id, index, X]` where X is pointing to which array of SRS names to use within
-            // srs_ids_stage_names, but from what I can tell... it doesn't matter? So I just use the local corrected names array.
-            const currentItemSrsIndex = subjectIdSrsList.find((subjectSrsInfo) => subjectSrsInfo[0] == currentItemId)[1];
-            const currentSrsStageName = CORRECTED_NAMES[currentItemSrsIndex];
-
-            const currentSrsTextDiv = document.querySelector(`div[class='character-header__current-srs-text']`);
-
-            // If doesn't exist, create a new container with the text element, otherwise change text.
-            if (currentSrsTextDiv == undefined) {
-                const currentSrsContainerHtml = `
+      // If doesn't exist, create a new container with the text element, otherwise change text.
+      if (currentSrsTextDiv == undefined) {
+        const currentSrsContainerHtml = `
                     <div class="character-header__current-srs-container" data-hidden=false>
                         <div class="character-header__current-srs-content">
                             <div class="character-header__current-srs-text">${currentSrsStageName}</div>
@@ -96,21 +154,28 @@
                     </div>
                 `;
 
-                const characterHeader = document.querySelector(`div[class='character-header__content']`);
-                characterHeader.insertAdjacentHTML('beforeend', currentSrsContainerHtml);
-            } else {
-                currentSrsTextDiv.textContent = currentSrsStageName;
-            }
-        }
+        const characterHeader = document.querySelector(
+          `div[class='character-header__content']`
+        );
+        characterHeader.insertAdjacentHTML(
+          "beforeend",
+          currentSrsContainerHtml
+        );
+      } else {
+        currentSrsTextDiv.textContent = currentSrsStageName;
+      }
     }
+  }
 
-    function getQuizQueueTarget(target) {
-        return clonedQueueElement.querySelector(`[data-quiz-queue-target="${target}"]`)
-    }
+  function getQuizQueueTarget(target) {
+    return clonedQueueElement.querySelector(
+      `[data-quiz-queue-target="${target}"]`
+    );
+  }
 
-    function addStyle() {
-        // Add CSS style for current SRS elements
-        const cssStyle = `
+  function addStyle() {
+    // Add CSS style for current SRS elements
+    const cssStyle = `
             .character-header__current-srs-container {
                 position:absolute;
                 bottom:10px;
@@ -158,14 +223,13 @@
             }
 
             .character-header__current-srs-container .character-header__current-srs-content {
-                background-color:var(--color-charcoal);
-                color:var(--color-quiz-srs-correct-text-color);
-                text-shadow:var(--color-quiz-srs-correct-text-shadow)
+                background-color: rgb(60, 60, 60);
+                color: rgb(144, 164, 174);
             }
         `;
-        const styleEl = document.createElement("style");
-        styleEl.id = "css_current-srs";
-        styleEl.textContent = cssStyle;
-        document.head.appendChild(styleEl);
-    }
+    const styleEl = document.createElement("style");
+    styleEl.id = "css_current-srs";
+    styleEl.textContent = cssStyle;
+    document.head.appendChild(styleEl);
+  }
 })();
